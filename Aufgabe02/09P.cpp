332a02e748af7077b1b944d52e194eadb8531d28
juffel <julian.dobmann@googlemail.com>
2012-10-10 14:22:14 +0200

    charFrequencies works

--- Aufgabe02/09P.cpp
+++ Aufgabe02/09P.cpp
@@ -1,19 +1,70 @@
 #include <iostream>
-#include <cstdio>
+#include <fstream>
 using namespace std;
+
+void printCharFreqs(int * freq);
+
 
 int main(int argc, char *argv[])
 {
-    const char * path = argv[1];
-    FILE * file;
-    file = fopen(path, "r");
-    if(file == NULL)
+    const char * path = argv[1]; 
+
+    ifstream infile;
+    infile.open(path, ifstream::in);
+    if (infile.is_open() != true)
     {
-        cout << "Konnte " << path << " nicht öffnen" << endl;
+        cout << "could not open " << path << endl;
         return -1;
-    } else
-    {
-        cout << path << " erfolgreich geöffnet." << endl;
     }
+
+    int charFreq [128] = { 0 }; // all elements 0
+
+    int c = infile.get();
+    while(infile.good())
+    {
+        if(c >= 0 && c <= 127)
+            charFreq[c]++;
+        c = infile.get();
+    }
+
+    infile.close();
+
+    printCharFreqs(charFreq);
+
     return 0;
+}
+
+
+void printCharFreqs(int * freq)
+{
+    // get highest frequency of letters
+    int max = 0;
+    for(int i = 65; i <= 90; i++)
+    {
+        if(freq[i] > max)
+            max = freq[i];
+    }
+    for(int i = 97; i <= 122; i++)
+    {
+        if(freq[i] > max)
+            max = freq[i];
+    }
+    // calculate appropriate scale so it fits on 80 char wide terminal
+    int scale = 1;
+    while(max/scale > 80)
+        scale *= 2;
+
+    // print
+    for(int i = 65; i <= 90; i++)
+    {
+        cout << (char) i << " ";
+        for(int j = 0; j < freq[i]/scale; j++) { cout << "|"; }
+        cout << endl;
+    }
+    for(int i = 97; i <= 122; i++)
+    {
+        cout << (char) i << " ";
+        for(int j = 0; j < freq[i]/scale; j++) { cout << "|"; }
+        cout << endl;
+    }
 }
