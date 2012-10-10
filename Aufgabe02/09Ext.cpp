c83f1a5e20bd986579d179a57ec1303ea6d40540
juffel <julian.dobmann@googlemail.com>
2012-10-10 15:04:58 +0200

    Aufgabe 09Ext/08 fast vollständig

---
+++ Aufgabe02/09Ext.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+using namespace std;
+
+void printCharFreqs(int * freq);
+void printCharFreqs(map<int, int> freq);
+
+int main(int argc, char *argv[])
+{
+    const char * path = argv[1]; 
+
+    ifstream infile;
+    infile.open(path, ifstream::in);
+    if (infile.is_open() != true)
+    {
+        cout << "could not open " << path << endl;
+        return -1;
+    }
+
+    //int charFreq [128] = { 0 }; // all elements 0
+    map<int, int> charFreq;
+
+    int c = infile.get();
+    while(infile.good())
+    {
+        if(c >= 65 && c <= 90 || c >= 97 && c <= 122)
+            charFreq[c]++;
+        c = infile.get();
+    }
+
+    infile.close();
+
+    printCharFreqs(charFreq);
+
+    return 0;
+}
+
+void printCharFreqs(map<int, int> freq)
+{   
+    map<int, int>::iterator it;
+    for(it = freq.begin(); it != freq.end(); it++)
+    {
+        cout << (char) (*it).first << " " << (*it).second << endl;
+    }
+}
+
+// functions argument is int array of length 128 with frequency of each char
+void printCharFreqs(int * freq)
+{
+    // get highest frequency of letters
+    int max = 0;
+    for(int i = 65; i <= 90; i++)
+    {
+        if(freq[i] > max)
+            max = freq[i];
+    }
+    for(int i = 97; i <= 122; i++)
+    {
+        if(freq[i] > max)
+            max = freq[i];
+    }
+
+    // calculate appropriate scale so it fits on 80 char wide terminal
+    int scale = 1;
+    while(max/scale > 80)
+        scale *= 2;
+
+    // print
+    for(int i = 65; i <= 90; i++)
+    {
+        cout << (char) i << " ";
+        for(int j = 0; j < freq[i]/scale; j++) { cout << "|"; }
+        cout << endl;
+    }
+    for(int i = 97; i <= 122; i++)
+    {
+        cout << (char) i << " ";
+        for(int j = 0; j < freq[i]/scale; j++) { cout << "|"; }
+        cout << endl;
+    }
+}
